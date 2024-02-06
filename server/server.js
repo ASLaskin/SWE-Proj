2b3d21b1bb9e4cb3e43e7bfbef1044636b16017d
ASLaskin <andrewlaskin@ufl.edu>
2024-02-06 12:33:18 -0500

    connected server to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,42 +1,82 @@
+require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors'); 
-const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const MONGODB_PASS = process.env.MongoDBPass;
+
+if (!MONGODB_PASS) {
+  console.error('MongoDB password is not provided in the environment variable.');
+  process.exit(1); // Exit the process if the password is not provided
+}
+
+const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Temporary data needs to be stored in database
-const users = [];
+const mongoURI = `mongodb+srv://SWEPassword1:${encodeURIComponent(MONGODB_PASS)}@cluster0.azznp8r.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get('/users', (req, res) => {
-  res.json(users);
+mongoose.connect(mongoURI, {
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+  
+const db = mongoose.connection;
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/users', async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = { name: req.body.name, password: hashedPassword };
-    users.push(user);
+    const newUser = new UserModel({
+      name: req.body.name,
+      password: hashedPassword,
+    });
+    await newUser.save();
     res.status(201).send();
-  } catch {
-    res.status(500).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 app.post('/users/login', async (req, res) => {
-  const user = users.find((user) => user.name === req.body.name);
-  if (user == null) {
-    return res.status(400).send('Cannot find user');
-  }
   try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
+    const user = await UserModel.findOne({ name: req.body.name });
+    if (!user) {
+      return res.status(400).send('Cannot find user');
+    }
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
+    if (isPasswordValid) {
       res.send('Success');
     } else {
       res.send('Not Allowed');
     }
-  } catch {
-    res.status(500).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
