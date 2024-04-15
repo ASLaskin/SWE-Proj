1404325885014cf10e63fd5a0a633bc9edc219d1
Xalan Dames <115499157+damesx04@users.noreply.github.com>
2024-04-15 17:22:35 -0400

    Smooth Swiping Animation Finalized

--- client/src/pages/swiping.jsx
+++ client/src/pages/swiping.jsx
@@ -1,58 +1,60 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import TinderCard from 'react-tinder-card';
 import './Swiping.css'; // Import your CSS file
 
 let string_arr=["Hello, World!", "Bruh", "please"];
 
-let global_ind=0;
-
 const Swiping = () => {
-  
-   const onSwipe = (direction) => {
-     console.log('You swiped: ' + direction)
-     global_ind+=1;
-   }
+  const [cards, setCards] = useState(string_arr);
+  const cardRefs = cards.map(() => React.createRef());
 
-  const swiped = (direction, nametoDelete) => {
-    // console.log("please")
-    console.log("global_ind")
-    global_ind += 1
-    console.log(global_ind)
+  const onSwipe = (direction, name) => {
+    console.log('You swiped: ' + direction + ' on ' + name);
+    setTimeout(() => {
+        setCards(cards => cards.filter(card => card !== name));
+      }, 600); // delay in ms
   }
 
-  const onCardLeftScreen = (myIdentifier) => {
-    console.log(myIdentifier + ' left the screen')
+  const onCardLeftScreen = (name) => {
+    console.log(name + ' left the screen');
+  }
+
+  const swipe = (dir) => {
+    const cardsLeft = cards.filter(card => !alreadyRemoved.includes(card))
+    if (cardsLeft.length) {
+      const toBeRemoved = cardsLeft[cardsLeft.length - 1] // Find the card object to be removed
+      const index = cards.map(card => card.name).indexOf(toBeRemoved.name) // Find the index of which to make the reference to
+      alreadyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+      cardRefs[index].current.swipe(dir) // Swipe the card!
+    }
   }
 
   return (
-    <div>
-      <div className="imageStack">
-
-      {/* <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>{string_arr[global_ind]}</TinderCard> */}
-
-
-      <TinderCard className='swipe'  onSwipe={(dir) => swiped(dir, string_arr[global_ind])} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
-      <div style={{ 
-                    backgroundImage: 'url(./src/assets/Solid_white_bordered.svg.jpeg)',
-                    backgroundPosition: 'center',
-                    }} className='card'>
-      <h3>{string_arr[global_ind]}</h3>
-      </div>
-      </TinderCard>
-      <TinderCard className='swipe'  onSwipe={(dir) => swiped(dir, string_arr[global_ind])} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
-      <div 
-      style={{ 
-            backgroundImage: 'url(./src/assets/Solid_white_bordered.svg.jpeg)',
-            backgroundPosition: 'center',
-            }} className='card'>
-      <h3>{string_arr[global_ind]}</h3>
-      </div>
-      </TinderCard>
-
-      </div> 
+    <div className="imageStack">
+      {cards.map((item, index) => (
+        <TinderCard
+          ref={cardRefs[index]}
+          key={item}
+          className='swipe'
+          onSwipe={(dir) => onSwipe(dir, item)}
+          onCardLeftScreen={() => onCardLeftScreen(item)}
+        >
+          <div
+            style={{
+              backgroundImage: 'url(./src/assets/Solid_white_bordered.svg.jpeg)',
+              backgroundPosition: 'center',
+              zIndex: cards.length - index, // Add this line
+            }}
+            className='card'
+          >
+            <h3>{item}</h3>
+          </div>
+        </TinderCard>
+      ))}
+      <button onClick={() => swipe('left')}></button>
+      <button onClick={() => swipe('right')}></button>
     </div>
   );
 }
-
 
 export default Swiping;
